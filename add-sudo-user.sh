2a20df1768dce9410af369a19d11d81041b5b945
Travis Brown <travis.brown@mckesson.com>
2018-11-26 16:32:28 -0600

    Script to add admin

---
+++ add-sudo-user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USER=$1
+KEY=$2
+
+SUDO_FILE=/etc/sudoers.d/91-admin-users
+
+adduser --disabled-password --gecos "" $USER && \
+mkdir /home/$USER/.ssh && \
+chmod 700 /home/$USER/.ssh && \
+echo "$KEY" > /home/$USER/.ssh/authorized_keys && \
+chmod 600 /home/$USER/.ssh/authorized_keys && \
+chown -R $USER:$USER /home/$USER/.ssh && \
+echo "$USER ALL=(ALL) NOPASSWD:ALL" >> $SUDO_FILE && \
+chmod 0440 $SUDO_FILE
